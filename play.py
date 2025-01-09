1cfdf28e7e82af0fd9880904d28111b83c737683
furriesRus <shousejackson@gmail.com>
2025-01-09 09:08:31 -0700

    added main file

---
+++ play.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+
+# Pygame initialization #
+pg.init()
+screen = pg.display.set_mode((1280, 720))
+clock = pg.time.Clock()
+running = True
+dt = 0
+
+
+while(running):
+    pg.exit()
