7069db55ae42fb2a0122c8172a4dd0f718fe531b
KamishiroRuiLover <iplayfo4@gmail.com>
2025-01-09 11:46:23 -0600

    added game and template, made play import game files

---
+++ game/template/functions/default.py
@@ -0,0 +1,29 @@
+from os import path
+import json
+
+
+def read_saves_dir(game):
+    saves = {
+        "slot1": {
+            "loaded": False
+        },
+        "slot2": {
+            "loaded": False
+        },
+        "slot3": {
+            "loaded": False
+        }
+    }
+
+    for i in ["sav1", "sav2", "sav3"]:
+        if path.exists("saves/" + game + "/" + i):
+            saves[i]["loaded"] = True
+    
+    return saves
+
+
+def test():
+    print("test")
+
+
+# end of file #
--- play.py
+++ play.py
@@ -1,4 +1,13 @@
 import pygame as pg
+import sys
+import json
+
+
+# arguments #
+if len(sys.argv) != 2:
+    print("Usage: python play.py GAME_NAME")
+    sys.exit(1)
+GAME = "game/" + sys.argv[1]
 
 
 # Pygame initialization #
@@ -9,8 +18,33 @@ running = True
 dt = 0
 
 
+# Misc globals #
+bg_color = pg.Color(0, 0, 0)
+
+
+# Loading files #
+execs = []
+def read_core(game):
+    file = open(game + "/core.json").read()
+    j_dict = json.loads(file)
+
+    pg.display.set_caption(j_dict["name"] + " - " + j_dict["author"] + " - " + j_dict["version"])
+
+    icon = pg.image.load(game + "/" + j_dict["icon"])
+    pg.display.set_icon(icon)
+
+    for i in j_dict["func_files"]:
+        file = open(game + "/" + i).read()
+        exec(file, globals())
+        # execs.append(file)
+
+read_core(GAME)
+
+globals
+
+
 while(running):
-    
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
