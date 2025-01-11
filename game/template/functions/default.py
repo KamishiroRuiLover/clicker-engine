50f32fdde78a6eb21c481df0658b424afa7baea1
KamishiroRuiLover <iplayfo4@gmail.com>
2025-01-10 20:39:02 -0600

    Added new default functions, added local variables  made stages name-based and removed stages.json

--- game/template/functions/default.py
+++ game/template/functions/default.py
@@ -4,6 +4,8 @@ import json
 
 # engine funcs #
 def read_saves_dir(param):
+    # as of now unused, may be used in the future
+
     saves = {
         "slot1": {
             "loaded": False
@@ -24,35 +26,73 @@ def read_saves_dir(param):
 
 
 def none(param):
+    #param "" (no parameters, just empty string)
+
     pass
 
 
 def exit(param):
+    #param "" (no parameters, just empty string)
+
     global running
     running = False
 
 
 def add_stage(param):
+    #param: "stage"
+
     global stages
     stages.append(param)
 
 
+def switch_world(param):
+    #param ["GAME", "world_name"] #GAME must be the first slot.
+
+    global read_world_func
+
+    read_world_func(param[0], param[1])
+
+
 # template/clicker project specific funcs #
 def inc_global_by_global(param):
-    # param = [var_name, inc_var_name] #
+    #param ["var_name", "var2_name"] #Slot 1 must be the variable you want to change, changed by the variable listed in slot 2. Both must be globals.
+
     global gbls
     gbls[param[0]] += gbls[param[1]]
 
 
 def purchase_stage(param):
-    # param = [stage, currency, cost]
+    # param ["stage", "currency"(global variable), cost(int)]
     global gbls
-    global stages
 
-    if gbls[param[1]] >= gbls[param[2]]:
-        stages.append(param[0])
+    if gbls[param[1]] >= param[2]:
+        add_stage(param[0])
 
         gbls[param[1]] -= param[2]
+
+
+def purchase_global_inc(param):
+    #param ["var_to_change", "inc_amount", "currency"(global variable), cost(int)]
+    global gbls
+
+    if gbls[param[2]] >= param[3]:
+        gbls[param[0]] += param[1]
+
+        gbls[param[2]] -= param[3]
+
+
+def set_global(param):
+    #param ["var_name", new_value(any value)]
+    global gbls
+
+    gbls[param[0]] = param[1]
+
+
+def set_local(param):
+    #param ["local_var_name", new_value(any value)]
+    global lcls
+
+    lcls[param[0]] = param[1]
 
 
 # end of file #
